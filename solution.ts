87ced4f50e9de2e95f043697cfc69fb3630549d4
Avinash Sinha <avinashsinha751@gmail.com>
2025-11-18 02:35:13 +0600

    problem 3

--- solution.ts
+++ solution.ts
@@ -19,7 +19,6 @@ function getLength(value: string | any[]): number {
   return 0;
 }
 
-
 class Person {
   name: string;
   age: number;
@@ -33,14 +32,4 @@ class Person {
     return `'Name: ${this.name}, Age: ${this.age}'`;
   }
 }
-
-const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
-
-const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
-
-
-
-
 
